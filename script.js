234e79c65bf8b9fc2d758957ad2ffabda6f95d7f
Maulik-009 <149244852+Maulik-009@users.noreply.github.com>
2023-12-10 00:30:06 +0530

    made computer-won work

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-function rps() {
-    let playerSelection = prompt("Enter choice");
-    const compChoices = ["Rock", "Paper", "Scissor"]
-    let computerSelection;
+let playerSelection = prompt("Enter choice");
+let playerSelectionLower = playerSelection.toLowerCase();
+const compChoices = ["Rock", "Paper", "Scissor"]
+let randNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+let compSelection = compChoices[randNum];
+let compSelectionLower = compSelection.toLowerCase();
+let playerSelNum;
+console.log(compSelectionLower);
+console.log(playerSelection);
+
+playRound();
+
+function playRound() {
+    convertToNum();
+
+    if(randNum === playerSelNum) {
+        console.log("Tie");
+    }
+
+    if (playerSelectionLower === "rock" && compSelectionLower === "paper") {
+        console.log("Computer won");
+    } 
+
+    else if (playerSelectionLower === "paper" && compSelectionLower === "scissor") {
+        console.log("Computer won");
+    } 
+
+    else if (playerSelectionLower === "scissor" && compSelectionLower === "rock") {
+        console.log("Computer won");
+    } 
+}
+
+function convertToNum() {
+    if (playerSelectionLower === "rock") {
+        playerSelNum = 0;
+    }
+
+    if (playerSelectionLower === "paper") {
+        playerSelNum = 1;
+    }
+
+    if (playerSelectionLower === "scissor") {
+        playerSelNum = 2;
+    }
 }
